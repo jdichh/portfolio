029d96b73c9bbaad1e5199652e6447572e515260
jdichh <jpsdichoso21@gmail.com>
2023-08-07 17:06:52 +0800

    updated mobile navbar behavior

--- js/mobileNav.js
+++ js/mobileNav.js
@@ -1,14 +1,23 @@
-const navLinks = document.querySelector('.navLinks')
-const toggleButton = document.querySelector('.toggleButton')
+const navLinks = document.querySelector(".navLinks");
+const toggleButton = document.querySelector(".toggleButton");
+const links = document.querySelectorAll(".navLinks a"); // select all links inside navLinks
+const dimmedBackground = document.querySelector(".dimmedBackground"); // select the dimmed background
 
-toggleButton.addEventListener('click', () => {
-    const visibility = navLinks.getAttribute('data-visible');
-    if (visibility === "false") {
-        navLinks.setAttribute('data-visible', true)
-        toggleButton.setAttribute('aria-expanded', true)
-    }
-    else if (visibility === "true"){
-        navLinks.setAttribute('data-visible', false)
-        toggleButton.setAttribute('aria-expanded', false)
-    }
+function toggleNavbar(visibility) {
+  navLinks.setAttribute("data-visible", visibility);
+  toggleButton.setAttribute("aria-expanded", visibility);
+  dimmedBackground.style.display = visibility === "true" ? "block" : "none"; // show or hide the dimmed background
+}
+
+toggleButton.addEventListener("click", () => {
+  const visibility =
+    navLinks.getAttribute("data-visible") === "false" ? "true" : "false";
+  toggleNavbar(visibility);
+});
+
+// add event listener to each link
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    toggleNavbar("false");
+  });
 });
